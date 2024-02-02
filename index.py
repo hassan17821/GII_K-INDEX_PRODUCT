91e497351a5f36ce5e6fd3de8effaad8026d720d
hassan17821 <hassaan@nowcasting.pk>
2024-02-02 11:56:02 +0500

    v3 -- custom colors

--- index.py
+++ index.py
@@ -3,13 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
-from matplotlib.colors import ListedColormap
+from matplotlib.colors import LinearSegmentedColormap
 
 # Read BUFR data
-df = pdbufr.read_bufr('drive/MyDrive/GII.bufr', columns=("latitude", "longitude", "kIndex", "koIndex", "precipitableWater"))
+# df = pdbufr.read_bufr('drive/MyDrive/GII.bufr', columns=("latitude", "longitude", "kIndex", "koIndex", "precipitableWater"))
 
 # Filter data based on latitude and longitude range
-filterd_df = df[
+filtered_df = df[
     (df['latitude'] >= 20)
     & (df['latitude'] <= 40)
     & (df['longitude'] >= 43.753)
@@ -29,22 +29,25 @@ ax.add_feature(cfeature.COASTLINE)
 ax.add_feature(cfeature.BORDERS, linestyle=':')
 ax.add_feature(cfeature.OCEAN, facecolor='lightblue')
 
-# Define discrete colormap using 'tab10'
-num_colors = 10
-discrete_cmap = plt.get_cmap('tab10', num_colors)
+# Define a custom colormap
+colors = [(0.8, 0, 0),(1, 0.5, 0.16),(1, 0, 0.5),(1, 1, 0.2), (0.29, 0.66, 0.16),(0.16, 0.37, 0.9)]  # Blue to Green to Yellow to Orange to Red
 
-# Plot the KIndex with discrete colors
+n_bins = 10  # Number of color bins
+cmap_name = 'custom_contrast'
+custom_cmap = LinearSegmentedColormap.from_list(cmap_name, colors, N=n_bins)
+
+# Plot the KIndex with custom colormap
 sc = ax.scatter(
-    filterd_df['longitude'],
-    filterd_df['latitude'],
-    c=filterd_df['kIndex'],
-    cmap=discrete_cmap,
+    filtered_df['longitude'],
+    filtered_df['latitude'],
+    c=filtered_df['kIndex'],
+    cmap=custom_cmap,
     transform=ccrs.PlateCarree(),
     s=1
 )
 
 # Add a colorbar
-cbar = plt.colorbar(sc, ticks=np.linspace(filterd_df['kIndex'].min(), filterd_df['kIndex'].max(), num_colors))
+cbar = plt.colorbar(sc, ticks=np.linspace(filtered_df['kIndex'].min(), filtered_df['kIndex'].max(), n_bins))
 cbar.set_label('K-Index')
 
 # Add a title
