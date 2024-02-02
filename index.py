1b768f193202f695acee9fe90a2aab21cb17332e
hassan17821 <hassaan@nowcasting.pk>
2024-02-02 11:59:31 +0500

    Update index.py

--- index.py
+++ index.py
@@ -30,7 +30,7 @@ ax.add_feature(cfeature.BORDERS, linestyle=':')
 ax.add_feature(cfeature.OCEAN, facecolor='lightblue')
 
 # Define a custom colormap
-colors = [(0.8, 0, 0),(1, 0.5, 0.16),(1, 0, 0.5),(1, 1, 0.2), (0.29, 0.66, 0.16),(0.16, 0.37, 0.9)]  # Blue to Green to Yellow to Orange to Red
+colors = [(0.8, 0, 0),(1, 0.5, 0.16),(1, 0, 0.7),(1, 1, 0.27), (0.29, 0.66, 0.16),(0.16, 0.37, 0.9)]  # Blue to Green to Yellow to Orange to Red
 
 n_bins = 10  # Number of color bins
 cmap_name = 'custom_contrast'
