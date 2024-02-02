02435d73d18094c5ce1371f9bac2ede8246a79b6
hassan17821 <hassaan@nowcasting.pk>
2024-02-02 12:43:11 +0500

    GII - kIndex -- 1

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-# GII - precipitableWater
+# GII - k-Index
 import pdbufr
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,8 +6,8 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 
 # Read BUFR data
-productKey = 'precipitableWater'
-productLabel = 'Precipitable Water'
+productKey = 'kIndex'
+productLabel = 'k-Index'
 
 #df = pdbufr.read_bufr('drive/MyDrive/GII.bufr', columns=("latitude", "longitude", "kIndex", "koIndex", "precipitableWater"))
 
@@ -17,8 +17,8 @@ filterd_df = df[
      & (df['latitude'] <= 40)
      & (df['longitude'] >= 50)
      & (df['longitude'] <= 90)
-    #  & (df[productKey] >= -30)
-    #  & (df[productKey] <= 36)
+     & (df[productKey] >= -30)
+     & (df[productKey] <= 36)
 ]
 
 print(filterd_df)
@@ -36,7 +36,7 @@ ax.add_feature(cfeature.OCEAN, facecolor='lightblue')
 # Plot the KIndex
 # ValueError: 'coolwadrm' is not a valid value for cmap; supported values are 'Accent', 'Accent_r', 'Blues', 'Blues_r', 'BrBG', 'BrBG_r', 'BuGn', 'BuGn_r', 'BuPu', 'BuPu_r', 'CMRmap', 'CMRmap_r', 'Dark2', 'Dark2_r', 'GnBu', 'GnBu_r', 'Greens', 'Greens_r', 'Greys', 'Greys_r', 'OrRd', 'OrRd_r', 'Oranges', 'Oranges_r', 'PRGn', 'PRGn_r', 'Paired', 'Paired_r', 'Pastel1', 'Pastel1_r', 'Pastel2', 'Pastel2_r', 'PiYG', 'PiYG_r', 'PuBu', 'PuBuGn', 'PuBuGn_r', 'PuBu_r', 'PuOr', 'PuOr_r', 'PuRd', 'PuRd_r', 'Purples', 'Purples_r', 'RdBu', 'RdBu_r', 'RdGy', 'RdGy_r', 'RdPu', 'RdPu_r', 'RdYlBu', 'RdYlBu_r', 'RdYlGn', 'RdYlGn_r', 'Reds', 'Reds_r', 'Set1', 'Set1_r', 'Set2', 'Set2_r', 'Set3', 'Set3_r', 'Spectral', 'Spectral_r', 'Wistia', 'Wistia_r', 'YlGn', 'YlGnBu', 'YlGnBu_r', 'YlGn_r', 'YlOrBr', 'YlOrBr_r', 'YlOrRd', 'YlOrRd_r', 'afmhot', 'afmhot_r', 'autumn', 'autumn_r', 'binary', 'binary_r', 'bone', 'bone_r', 'brg', 'brg_r', 'bwr', 'bwr_r', 'cividis', 'cividis_r', 'cool', 'cool_r', 'coolwarm', 'coolwarm_r', 'copper', 'copper_r', 'cubehelix', 'cubehelix_r', 'flag', 'flag_r', 'gist_earth', 'gist_earth_r', 'gist_gray', 'gist_gray_r', 'gist_heat', 'gist_heat_r', 'gist_ncar', 'gist_ncar_r', 'gist_rainbow', 'gist_rainbow_r', 'gist_stern', 'gist_stern_r', 'gist_yarg', 'gist_yarg_r', 'gnuplot', 'gnuplot2', 'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r', 'hot', 'hot_r', 'hsv', 'hsv_r', 'inferno', 'inferno_r', 'jet', 'jet_r', 'magma', 'magma_r', 'nipy_spectral', 'nipy_spectral_r', 'ocean',...
 
-sc = ax.scatter(filterd_df['longitude'], filterd_df['latitude'], c=filterd_df[productKey], cmap='Accent', transform=ccrs.PlateCarree(), s=1)
+sc = ax.scatter(filterd_df['longitude'], filterd_df['latitude'], c=filterd_df[productKey], cmap='RdYlBu', transform=ccrs.PlateCarree(), s=1)
 
 # Add a colorbar
 cbar = plt.colorbar(sc)
