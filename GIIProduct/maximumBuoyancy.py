adaa451b2445c8d7c263844daaae3e89a3b6ac52
hassan17821 <hassaan@nowcasting.pk>
2024-02-15 13:46:22 +0500

    Adding maxBoyancy

---
+++ GIIProduct/maximumBuoyancy.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from matplotlib.colors import Normalize, ListedColormap, LinearSegmentedColormap
+
+def plot_maximumBuoyancy(df, outputPath):
+    latBound = [7.22, 37.454]
+    lngBound = [43.753, 102.363]
+    productKey = '#1#maximumBuoyancy'
+    cmap = 'Spectral_r'
+    # Filter data based on latitude and longitude range
+    filtered_df = df[
+        (df['#1#latitude'] >= latBound[0])
+        & (df['#1#latitude'] <= latBound[1])
+        & (df['#1#longitude'] >= lngBound[0])
+        & (df['#1#longitude'] <= lngBound[1])
+    ]
+
+    # Create the base map
+    fig = plt.figure(figsize=(16, 16))
+    ax = plt.axes(projection=ccrs.Mercator())
+    ax.set_extent([lngBound[0], lngBound[1], latBound[0], latBound[1]], crs=ccrs.PlateCarree())
+
+    sc = ax.scatter(filtered_df['#1#longitude'], filtered_df['#1#latitude'], c=filtered_df[productKey], norm=norm, cmap=cmap, transform=ccrs.PlateCarree(), s=1)
+
+    fig.savefig(outputPath, format='webp', dpi=300, bbox_inches='tight', pad_inches=0)
+
+export = plot_maximumBuoyancy
