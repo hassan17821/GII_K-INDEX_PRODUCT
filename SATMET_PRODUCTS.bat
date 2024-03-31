bc71bbcbb7d5092d51535394330fe962bebdd3b5
hassan17821 <hassaan@nowcasting.pk>
2024-03-31 14:44:14 +0500

    Update SATMET_PRODUCTS.bat

--- SATMET_PRODUCTS.bat
+++ SATMET_PRODUCTS.bat
@@ -10,10 +10,6 @@ set CONDA_ENV=base
 :: Set the Python script file path
 set python_script=D:/SATMET_PRODUCTS/GII_PRODUCT/SatpyProduct/index.py
 
-@REM :LOOP
-@REM :: Activate Conda environment
-@REM call "%CONDA_PATH%\Scripts\activate.bat" %CONDA_ENV%
-
 :: Set the current date and time
 for /f "delims=" %%b in ('"powershell [DateTime]::Now.AddHours(-5).ToString('HH-mm')"') do set mytime=%%b
 set time=%mytime%
@@ -22,21 +18,27 @@ set date=%mydate%
 set "destination_folder=D:/server1/Archive/%mydate%"
 if not exist "!destination_folder!" mkdir "!destination_folder!"
 
-:: Iterate over HH-mm folders within the specified date
+:: Create an array to store source drive paths
+set /a count=0
 for /d %%D in ("Z:/Data/XRIT/Archive/MSG2_IODC/%mydate%/*") do (
-    :: Check if the folder name is in the format of HH-mm
     for /f "tokens=1,* delims=-" %%X in ("%%~nxD") do (
         set "hhmm=%%X-%%Y"
-        set "source_drive=Z:/Data/XRIT/Archive/MSG2_IODC/%mydate%/!hhmm!"
-        if exist "!source_drive!" (
-            python %python_script% "!date!" "!hhmm!" "!source_drive!" "!destination_folder!"
-        )
+        set "source_drive[!count!]=Z:/Data/XRIT/Archive/MSG2_IODC/%mydate%/!hhmm!"
+        set /a count+=1
+    )
+)
+
+:: Loop over the source drive paths in reverse order
+for /l %%i in (%count%,-1,0) do (
+    set "source_drive=!source_drive[%%i]!"
+    if exist "!source_drive!" (
+        python %python_script% "!date!" "!hhmm!" "!source_drive!" "!destination_folder!"
     )
 )
 
 :: Deactivate Conda environment
-@REM call "%CONDA_PATH%\Scripts\deactivate.bat"
+:: call "%CONDA_PATH%\Scripts\deactivate.bat"
 
 :: Wait for 5 minutes before the next iteration
 timeout /t 300 /nobreak
-goto LOOP
+goto :EOF
