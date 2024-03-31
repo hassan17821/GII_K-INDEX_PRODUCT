d9bb641d2cdce66d571ee8e7e46dcd0e8931c6d7
hassan17821 <hassaan@nowcasting.pk>
2024-04-01 04:40:32 +0500

    Update constants.py

--- SatpyProduct/constants.py
+++ SatpyProduct/constants.py
@@ -1,11 +1,20 @@
 from utils import generate24HourTimeRange,generateDayTimeRange ,generateNightTimeRange
 
+day_and_night_products = [
+    { "product_key": "dust" ,"product_title": 'Dust_RGB', "product_type": 'product' , "valid_time_args": generate24HourTimeRange()},
+]
+
 day_products = [
     { "product_key": "fog" ,"product_title": 'Fog', "product_type": 'product' , "valid_time_args": generateDayTimeRange()},
+    { "product_key": "hrv_fog" ,"product_title": 'HRV_Fog', "product_type": 'product' , "valid_time_args": generateDayTimeRange()},
+    { "product_key": "snow" ,"product_title": 'Snow_RGB', "product_type": 'product' , "valid_time_args": generateDayTimeRange()},
+    { "product_key": "hrv_severe_storms" ,"product_title": 'HRV_Severe_Storms', "product_type": 'product' , "valid_time_args": generateDayTimeRange()},
+    { "product_key": "day_microphysics" ,"product_title": 'Microphysics', "product_type": 'product' , "valid_time_args": generateDayTimeRange()},
 ]
 
 night_products = [
     { "product_key": "night_fog" ,"product_title": 'Fog', "product_type": 'product' , "valid_time_args": generateNightTimeRange()},
+    { "product_key": "night_microphysics" ,"product_title": 'Microphysics', "product_type": 'product' , "valid_time_args": generateNightTimeRange()},
 ]
 
 band_products = [
@@ -21,8 +30,9 @@ band_products = [
     { "product_key": "VIS008" ,"product_title": 'VIS008', "product_type": 'band' , "valid_time_args": generate24HourTimeRange()},
     { "product_key": "WV_062" ,"product_title": 'WV_062', "product_type": 'band' , "valid_time_args": generate24HourTimeRange()},
     { "product_key": "WV_073" ,"product_title": 'WV_073', "product_type": 'band' , "valid_time_args": generate24HourTimeRange()},
+
 ]
 
-products= band_products + night_products + day_products
+products= band_products + day_and_night_products + night_products + day_products
     
 export = products
