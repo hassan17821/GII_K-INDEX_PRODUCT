ed2e76231f82144b30c64be080ce9672b09009d0
hassan17821 <hassaan@nowcasting.pk>
2024-04-15 11:41:20 +0500

    Update constants.py

--- SatpyProduct/constants.py
+++ SatpyProduct/constants.py
@@ -37,6 +37,6 @@ band_products = [
 
 ]
 
-products=  day_and_night_products + night_products + day_products
+products=  band_products + day_and_night_products + night_products + day_products;
     
 export = products
