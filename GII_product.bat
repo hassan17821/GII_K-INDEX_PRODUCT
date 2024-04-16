186e79cad56bd696a250aee74d70fe1d996b3219
hassan17821 <hassaan@nowcasting.pk>
2024-04-16 15:48:09 +0500

    Update GII_product.bat

--- GII_product.bat
+++ GII_product.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 
+:START_POINT
 :: Set the path to your Conda installation
 set CONDA_PATH=C:\Softwares\miniconda
 
@@ -9,8 +10,8 @@ set CONDA_ENV=base
 
 :: Set the Python script file path
 set python_script=D:/SATMET_PRODUCTS/GII_PRODUCT/GIIProduct/index.py
+set "input_dir=//EUMETCAST-INGES/Data/XRIT/Binary files/Archive"
 
-:LOOP
 :: Activate Conda environment
 call "%CONDA_PATH%\Scripts\activate.bat" %CONDA_ENV%
 
@@ -19,25 +20,42 @@ for /f "delims=" %%b in ('"powershell [DateTime]::Now.AddHours(-5).ToString('HH-
 set time=%mytime%
 for /f "delims=" %%a in ('"powershell [DateTime]::Now.AddHours(-5).ToString('yyyy-MM-dd')"') do set mydate=%%a
 set date=%mydate%
+@REM set "destination_folder=//eumetcast-proc/images/Archive/Weather/uploadsWG/Archive/%mydate%"
 set "destination_folder=D:/server1/Archive/%mydate%"
 if not exist "!destination_folder!" mkdir "!destination_folder!"
 
-:: Iterate over HH-mm folders within the specified date
-for /d %%D in ("S:/Archive/%mydate%/*") do (
-    :: Check if the folder name is in the format of HH-mm
+:: Create an array to store source drive paths
+set /a count=0
+for /d %%D in ("!input_dir!/%mydate%/*") do (
     for /f "tokens=1,* delims=-" %%X in ("%%~nxD") do (
-        set "hhmm=%%X-%%Y"
-        set "source_drive=S:/Archive/%mydate%/!hhmm!/GII.bufr"
-        if exist "!source_drive!" (
-           :: start "" cmd /c python %python_script% "!date!" "!hhmm!" "!source_drive!" "!destination_folder!"
-            python %python_script% "!date!" "!hhmm!" "!source_drive!" "!destination_folder!"
-        )
+        set "hhmm[!count!]=%%X-%%Y"
+        set "source_drive[!count!]=!input_dir!/%mydate%/%%X-%%Y/GII.bufr"
+        set /a count+=1
     )
 )
 
+:: Loop over the source drive paths in reverse order
+set "temp_count=0"
+set "max_bounds=10"
+for /l %%i in (%count%,-1,0) do (
+    echo "i = %%i"
+    set "source_drive=!source_drive[%%i]!"
+    set "hhmm=!hhmm[%%i]!"
+    if exist "!source_drive!" (
+        echo "%python_script% "!date!" "!hhmm!" "!source_drive!" "!destination_folder!"
+        python %python_script% "!date!" "!hhmm!" "!source_drive!" "!destination_folder!"
+        set /a temp_count+=1
+        if !temp_count! gtr !max_bounds! (
+            goto :break_loop
+        )
+    )
+)
+:break_loop
+echo "Loop terminated because temp count = %temp_count%." 
+
 :: Deactivate Conda environment
-@REM call "%CONDA_PATH%\Scripts\deactivate.bat"
+:: call "%CONDA_PATH%\Scripts\deactivate.bat"
 
 :: Wait for 5 minutes before the next iteration
-timeout /t 300 /nobreak
-goto LOOP
+timeout /t 20 /nobreak
+goto :START_POINT
