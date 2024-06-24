8c8e9800029fc2f511a362181d159e9911112d37
hassan17821 <hassaan@nowcasting.pk>
2024-06-24 15:48:37 +0500

    ...

---
+++ AVMAP_HIGH.bat
@@ -0,0 +1,5 @@
+@echo on
+
+CALL cmd /k python "D:\SATMET_PRODUCTS\GII_PRODUCT\AvMapProduct\transparent_avmap_high\index.py"
+
+timeout /t 1 /nobreak >nul
---
+++ TransparentBlackBG.bat
@@ -0,0 +1,5 @@
+@echo on
+
+CALL cmd /k python "D:\SATMET_PRODUCTS\black_bg_transparent.py"
+
+timeout /t 1 /nobreak >nul
